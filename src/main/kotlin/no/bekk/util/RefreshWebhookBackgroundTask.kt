a2ac86412b446619a51fa0214f43a416b990ca05
JegHeterGyda <100707649+JegHeterGyda@users.noreply.github.com>
2026-05-05 12:14:11 +0200

    only non-webhook-airtable-schemas uses cache to fetch form (#139)

--- src/main/kotlin/no/bekk/util/RefreshWebhookBackgroundTask.kt
+++ src/main/kotlin/no/bekk/util/RefreshWebhookBackgroundTask.kt
@@ -4,15 +4,19 @@ import io.ktor.server.application.*
 import kotlinx.coroutines.*
 import no.bekk.providers.AirTableProvider
 import no.bekk.services.FormService
+import kotlin.time.Duration.Companion.hours
 
 fun Application.configureBackgroundTasks(formService: FormService) {
     launchBackgroundTask {
         while (isActive) {
             try {
                 val airTableProviders = formService.getFormProviders().filterIsInstance<AirTableProvider>()
+                var hasProvidersWithoutWebhook = false
                 airTableProviders.forEach { provider ->
                     if (provider.webhookId.isNullOrEmpty()) {
-                        logger.info("Table ${provider.id} has no webhook id. Nothing to refresh.")
+                        logger.info("Table ${provider.id} has no webhook id. Updating cache instead.")
+                        provider.updateCaches()
+                        hasProvidersWithoutWebhook = true
                         return@forEach
                     }
                     val success = provider.refreshWebhook()
@@ -24,8 +28,9 @@ fun Application.configureBackgroundTasks(formService: FormService) {
                     provider.updateCaches() // temp solution while webhooks don't work at SKIP
                 }
 
-                // Delay for 24 hours
-                delay(1 * 60 * 60 * 1000L) // changed to 1 hour while webhooks are not working as intended
+                // Delay for 1 hour if one provider uses cache, and 24 hours if all providers uses webhooks.
+                val delayMillis = if (hasProvidersWithoutWebhook)  1.hours else 24.hours
+                delay(delayMillis)
             } catch (e: CancellationException) {
                 throw e
             } catch (e: Exception) {
